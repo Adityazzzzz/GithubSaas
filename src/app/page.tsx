25fe631b682fa009e8eb781730e24aa79f30108d
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-11-27 16:02:35 +0530

    clerk done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
-    
+    return (<>
+        <h1>hello</h1>
+        <Button>click</Button></>
+    )  
 }

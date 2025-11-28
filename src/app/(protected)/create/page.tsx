25111d9b237dcd8f15d39211e511660d37f7561d
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-11-28 20:30:35 +0530

    errors started :(

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import github from '@/icon/undraw_developer-activity_4zqd.svg'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { api } from '@/trpc/react'
+import { toast } from 'sonner'
 type FormInput = {
     repoUrl: string
     projectName: string
@@ -12,9 +14,23 @@ type FormInput = {
 
 const CreatePage = () => {
     const { register, handleSubmit, reset } = useForm<FormInput>()
+    const createProject = api.project.createProject.useMutation()
 
     function onSubmit(data:FormInput){
-        window.alert(JSON.stringify(data,null,2))
+        // window.alert(JSON.stringify(data,null,2))
+        createProject.mutate({
+            githubUrl:data.repoUrl,
+            name:data.projectName,
+            githubToken:data.githubToken,
+        },{
+            onSuccess:()=>{
+                toast.success('Project created successfully')
+                reset()
+            },
+            onError:()=>{
+                toast.error('Failed to create Project')
+            }
+        })
         return true
     }
     return (

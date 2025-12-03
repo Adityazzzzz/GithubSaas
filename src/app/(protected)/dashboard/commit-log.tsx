128fa1e01667018400b08104d73e9dd0030890ce
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-03 17:50:24 +0530

    UI- enhaced

--- src/app/(protected)/dashboard/commit-log.tsx
+++ src/app/(protected)/dashboard/commit-log.tsx
@@ -2,10 +2,12 @@
 import useProject from '@/hooks/use-project'
 import { cn } from '@/lib/utils'
 import { api } from '@/trpc/react'
+import { ExternalLink } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const CommitLog = () => {
-    const { projectId } = useProject()
+    const { projectId,project } = useProject()
     const { data: commits } = api.project.getCommits.useQuery({ projectId })
 
     return (
@@ -18,7 +20,24 @@ const CommitLog = () => {
                 )}>
                     <div className='w-px translate-x-1 bg-gray-200'></div>
                 </div>
-                
+
+                <>
+                <img src={commit.commitAuthorAvatar} alt='commit avatar' className='relative mt-4 size-8 flex-name rounded-full bg-gray-50'/>
+                <div className='flex-auto rounded-mg bg-white p-3 ring-1 ring-inset ring-gray-200'>
+                    <div className='flex justify-between gap-4'>
+                        <Link target='_blank' href={`${project?.githubUrl}/commit/${commit.commitHash}`} className='py-0.5 text-xs leading-5 text-gray-500'>
+                            <span className='font-medium text-gray-900'>
+                                {commit.commitAuthorName}
+                            </span>{" "}
+                            <span className='inline-flex items-center'>
+                                commited
+                                <ExternalLink className='ml-1 size-4' />
+                            </span>
+                        </Link>
+                    </div>
+
+                </div>
+                </>
             </li>
         })}
         </ul>

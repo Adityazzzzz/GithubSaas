4507d2c85ce72d7961214c3aa5c5314d4036df2d
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-07 18:09:28 +0530

    ask ai for specifc page

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -9,44 +9,44 @@ import Image from 'next/image'
 import React from 'react'
 
 const AskQuestionCard = () => {
-  const {project,projectId} = useProject()
-  const [open,setOpen] = React.useState(false)
-  const [question,setQuestion] = React.useState('')
+    const {project,projectId} = useProject()
+    const [open,setOpen] = React.useState(false)
+    const [question,setQuestion] = React.useState('')
 
-  const onSubmit = async(e:React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault()
-    window.alert(question)
-  }
+    const onSubmit = async(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        window.alert(question)
+    }
 
-  return <>
-    <Dialog open={open} onOpenChange={setOpen}>
-        <DialogHeader>
-            <DialogTitle>
-                {/* <Image src={} alt='' height={40} width={40}/> */}
+    return <>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogHeader>
+                <DialogTitle>
+                    {/* <Image src={} alt='' height={40} width={40}/> */}
 
-            </DialogTitle>
-        </DialogHeader>
-        <DialogContent>
+                </DialogTitle>
+            </DialogHeader>
+            <DialogContent>
 
-        </DialogContent>
-    </Dialog>
-    <Card className='relative col-span-3'>
-        <CardHeader>
-            <CardTitle>Ask a question</CardTitle>
-        </CardHeader>
-        <CardContent>
-            <form onSubmit={onSubmit}>
-                <Textarea placeholder='Which file should I edit to change the home page?'/>
-                <div className='h-4'>
+            </DialogContent>
+        </Dialog>
+        <Card className='relative col-span-3'>
+            <CardHeader>
+                <CardTitle>Ask a question</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form onSubmit={onSubmit}>
+                    <Textarea placeholder='Which file should I edit to change the home page?'/>
+                    <div className='h-4'>
 
-                </div>
-                <Button type='submit'>
-                    Ask GitBrain!
-                </Button>
-            </form>
-        </CardContent>
-    </Card>
-  </>
+                    </div>
+                    <Button type='submit'>
+                        Ask GitBrain!
+                    </Button>
+                </form>
+            </CardContent>
+        </Card>
+    </>
 }
 
 export default AskQuestionCard

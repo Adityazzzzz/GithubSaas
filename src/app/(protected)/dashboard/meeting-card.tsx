ba65557049e2e72c998f0cffb7e41df4e57caf75
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-11 13:58:30 +0530

    update firebase

---
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -0,0 +1,60 @@
+'use client'
+import {CircularProgressbar,buildStyles} from 'react-circular-progressbar'
+import { Card } from '@/components/ui/card'
+import { useDropzone } from 'react-dropzone'
+import React from 'react'
+import { uploadFile } from '@/lib/firebase'
+import { Presentation, Upload } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+
+const MeetingCard = () => {
+    const [isUploading, setIsUploading] = React.useState(false)
+    const [progress, setProgress] = React.useState(0)
+    const { getRootProps, getInputProps, isDragAccept } = useDropzone({
+        accept: {
+            'audio/*': ['.mp3', '.wav', '.m4a']
+        },
+        multiple: false,
+        maxSize: 50_000_000,
+        onDrop: async acceptedFiles => {
+            setIsUploading(true)
+            console.log(acceptedFiles)
+            const file = acceptedFiles[0]
+            const downloadUrl = await uploadFile(file as File,setProgress)
+            window.alert(downloadUrl)
+            setIsUploading(false)
+        }
+    })
+    return (
+        <Card className='col-span-1 flex flex-col items-center justify-center p-10' {...getRootProps()}>
+            {!isUploading && (
+                <>
+                <Presentation className="h-10 w-10 animate-bounce" />
+                <h3 className="mt-2 text-sm font-semibold text-gray-900">
+                    Create a new meeting
+                </h3>
+                <p className="mt-1 text-center text-sm text-gray-500">
+                    Analyse your meeting with GitBrain.
+                    <br />
+                    Powered by AI.
+                </p>
+                <div className="mt-6">
+                    <Button disabled={isUploading}>
+                        <Upload className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+                        Upload Meeting
+                        <input className="hidden" {...getInputProps()} />
+                    </Button>
+                </div>
+                </>
+            )}
+
+            {isUploading && (
+                <div className='flex items-center justify-center'>
+                    <CircularProgressbar value={progress} text={`${progress}%`} className='size-20'/>
+                    <p className='text-sm text-gray-500 text-center'>Uploading your meeting...</p>
+                </div>
+            )}
+        </Card>
+    )
+}
+export default MeetingCard

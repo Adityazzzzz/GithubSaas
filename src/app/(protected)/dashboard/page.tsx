29923a0f80f43884474bc05e47627e11c71babf1
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-15 14:09:07 +0530

    Archieve Button

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 import CommitLog from './commit-log'
 import AskQuestionCard from './ask-question-card'
 import MeetingCard from './meeting-card'
+import ArchiveButton from './archieve-button'
 
 const DashboardPage = () => {
     const {project} = useProject()
@@ -34,7 +35,7 @@ const DashboardPage = () => {
                 <div className='flex items-center gap-4'>
                     TeamMembers
                     InviteButton
-                    ArchiveButton
+                    <ArchiveButton/>
                 </div>
             </div>
 

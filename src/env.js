24864a98a84f9e1a013070f7448b6f3ac3904ba0
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-21 11:52:18 +0530

    Testing deployment

--- src/env.js
+++ src/env.js
@@ -2,31 +2,25 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here.
-   */
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    GITHUB_TOKEN: z.string(), // <--- 1. ADD THIS HERE
+    
+    GITHUB_TOKEN: z.string(),
+    GEMINI_API_KEY: z.string(),
   },
 
-  /**
-   * Specify your client-side environment variables schema here.
-   */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
-
-  /**
-   * You can't destruct `process.env` as a regular object...
-   */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN, // <--- 2. ADD THIS HERE
+    
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   },
   
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
